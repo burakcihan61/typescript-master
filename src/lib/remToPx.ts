c58e1758bba21ff432f33de900c2664d68586607
Burak Cihan <burakcihan622@gmail.com>
2023-12-11 16:37:50 +0300

    Update remToPx.ts

--- src/lib/remToPx.ts
+++ src/lib/remToPx.ts
@@ -1,3 +1,14 @@
+/**
+ * Converts a value from rem units to pixels.
+ *
+ * @param remValue - The value in rem units that needs to be converted to pixels.
+ * @returns The equivalent value of `remValue` in pixels.
+ *
+ * @example
+ * const remValue = 2;
+ * const pxValue = remToPx(remValue);
+ * console.log(pxValue); // Output: 32
+ */
 export function remToPx(remValue: number) {
   let rootFontSize =
     typeof window === "undefined"
